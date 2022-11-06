ac7ba2519dc94bf2646d60bdd59a9ebe747a79f4
kikoonishi <konishi@noctrl.edu>
2022-11-06 11:54:22 -0600

    Added header file

---
+++ Sales.h
@@ -0,0 +1,21 @@
+//Author: weCode
+//Sales.h
+
+#include <iostream>
+#include <string>
+#include<Customer.h>
+#include <Order.h>
+
+class Sales :private Customer{
+    private:
+        Order order; //order: product, service, ordername
+
+    public:
+        Sales(); //default constructor
+
+        //setter
+        void setOrder(Order);
+
+        //getter
+        Order getOrder();      
+};
