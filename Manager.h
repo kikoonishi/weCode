e21caaf3f82806b84fd76072939f9db2085a1b91
kikoonishi <konishi@noctrl.edu>
2022-11-06 11:55:19 -0600

    Added header file

---
+++ Manager.h
@@ -0,0 +1,14 @@
+//Author: weCode
+//Manager.h
+
+#include <Sales.h>
+
+class Manager :private Sales{
+    public:
+        Manager(); //default constructor
+
+        //might want to create new class that edit create remove products/services
+        //indead of accessing product and service classes directly?
+        
+        
+};
