80977e281c3c036ecb080c71ff893ab59db9feda
kikoonishi <konishi@noctrl.edu>
2022-11-07 15:30:12 -0600

    added file

---
+++ ProductManager.h
@@ -0,0 +1,27 @@
+//Author: weCode
+//ProductManager.h
+
+#include <Product.h>
+#include <Service.h>
+
+class ProductManager {//could be done in product/service class?
+    private:
+        Product product;
+        Service service;
+    public:
+        ProductManager();
+        ProductManager(Product);
+        ProductManager(Service);
+
+        Product getProduct();
+        Service getService();
+
+        //add,delete,edit product/service
+        void addProduct(Product);
+        void addService(Service);
+        void deleteProduct(Product);
+        void deleteService(Service);
+        void editProduct(Product);
+        void editService(Service);
+
+};
