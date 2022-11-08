1ef5fb8926356c250abf0a929ea9542683291da4
kikoonishi <konishi@noctrl.edu>
2022-11-08 14:57:23 -0600

    modified

--- Manager.h
+++ Manager.h
@@ -2,15 +2,14 @@
 //Manager.h
 
 #include <Sales.h>
-#include <ProductManager.h>
 
 class Manager :public Sales {
     public:
         using Sales::Sales; //inherit constructor
 
         //add,delete,edit product/service
-        void addProduct(Product);
-        void addService(Service);
+        void addProduct(std::string, double);
+        void addService(std::string, double);
         void deleteProduct(Product);
         void deleteService(Service);
         void editProduct(Product);
