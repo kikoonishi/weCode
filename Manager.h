454d12fd7cf62226f01e0dfbf7fc4839f26d491f
kikoonishi <93617903+kikoonishi@users.noreply.github.com>
2022-12-01 17:47:57 -0600

    Update Manager.h

--- Manager.h
+++ Manager.h
@@ -1,18 +1,41 @@
 //Author: weCode
 //Manager.h
 
+#include <string>
 #include "Sales.h"
+#include "Product.h"
+#include "Service.h"
 
+template <class T>
 class Manager :public Sales {
+    private:
+        Product product;//need be deleted
+        Service service;//need
+        static Database<T> productData;
+        static Database<T> serviceData;
+        
     public:
-        using Sales::Sales; //inherit constructor
+        Manager():Sales();//default constructor
+        Manager(std::string):Sales(std::string);//?
 
-        //add,delete,edit product/service
+        //TODO receive database as parameter
+        //add,delete,edit product/service in database
         void addProduct(std::string, double);
         void addService(std::string, double);
-        void deleteProduct(Product);
-        void deleteService(Service);
-        void editProduct(Product);
-        void editService(Service);
-        
+        void deleteProduct(std::string);//delete product by name
+        void deleteService(std::string);//delete service by name
+        void editProduct(std::string, double);//search by name and change price
+        void editService(std::string, double);
+
+/*
+in main
+Product P(something);
+productData.add(P);
+*/
+
+/*
+change customer/name or both
+ change product (newProduct)
+  change service (newService)
+*/
 };
