305981ff7b4d3a31f594db58cff3143e19ac959a
kikoonishi <93617903+kikoonishi@users.noreply.github.com>
2022-12-04 15:26:14 -0600

    Update Manager.h

--- Manager.h
+++ Manager.h
@@ -5,37 +5,24 @@
 #include "Sales.h"
 #include "Product.h"
 #include "Service.h"
+#include "Database.h"
 
 template <class T>
 class Manager :public Sales {
-    private:
-        Product product;//need be deleted
-        Service service;//need
-        static Database<T> productData;
-        static Database<T> serviceData;
-        
     public:
-        Manager():Sales();//default constructor
-        Manager(std::string):Sales(std::string);//?
+        //default constructor?????
+        Manager();
+        Manager(): Sales();
+        Manager(std::string, std::string, Account) :Sales(std::string, std::string, Account);//newID, type, userAccount
 
-        //TODO receive database as parameter
         //add,delete,edit product/service in database
-        void addProduct(std::string, double);
-        void addService(std::string, double);
-        void deleteProduct(std::string);//delete product by name
-        void deleteService(std::string);//delete service by name
-        void editProduct(std::string, double);//search by name and change price
-        void editService(std::string, double);
+        void addProduct(Database<T>, Product);        
+        void addService(Database<T>, Service);
+        void deleteProduct(Database<T>, Product);//delete product by Product
+        void deleteProduct(Database<T>, std::string);//delete product by product name
+        void deleteService(Database<T>, Service);//delete product by Service
+        void deleteService(Database<T>, std::string);//delete service by service name
+        void modifyProduct(Database<T>, Product, Product);//existring Product to new Product
+        void modifyService(Database<T>, Service, Service);//existring Service to new Service
 
-/*
-in main
-Product P(something);
-productData.add(P);
-*/
-
-/*
-change customer/name or both
- change product (newProduct)
-  change service (newService)
-*/
 };
