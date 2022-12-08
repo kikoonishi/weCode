febbb04942c65c495275498763ccbb745d71d24b
atrafacz <alexrafacz@gmail.com>
2022-12-08 08:15:17 -0600

    Updated Address.h

--- Address.h
+++ Address.h
@@ -1,30 +1,33 @@
 // Address.h
+#pragma once
+
 #include <iostream>
 #include <string>
 
 class Address {
-    private:
-        std::string streetAddress; // primary street address
-        std::string city;
-        std::string state;  // only if country is the United States
-        std::string areaCode;
-        std::string country;
-    public:
-        Address();  // default constructor
-        Address(std::string, std::string, std::string, std::string, std::string);   // overloaded constructor
+private:
+    std::string streetAddress; // primary street address
+    std::string city;
+    std::string state;  // only if country is the United States
+    std::string areaCode;
+    std::string country;
+public:
+    Address();  // default constructor
+    Address(std::string, std::string, std::string, std::string, std::string);   // overloaded constructor
 
-        // mutators / setters
-        void setStreetAddress(std::string);
-        void setCity(std::string);
-        void setState(std::string);
-        void setAreaCode(std::string);
-        void setCountry(std::string);
+    // mutators / setters
+    void setStreetAddress(std::string);
+    void setCity(std::string);
+    void setState(std::string);
+    void setAreaCode(std::string);
+    void setCountry(std::string);
 
-        // accessors / getters
-        std::string getStreetAddress();
-        std::string getCity();
-        std::string getState();
-        std::string getAreaCode();
-        std::string getCountry();
-        std::string toString(); // for printing an instance of Address
+    // accessors / getters
+    std::string getStreetAddress() const;
+    std::string getCity() const;
+    std::string getState() const;
+    std::string getAreaCode() const;
+    std::string getCountry() const;
+        
+    std::string toString(); // for printing an instance of Address
 };
