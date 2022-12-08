56a8e011d4ede2840853511ec087190b7ffde7af
atrafacz <alexrafacz@gmail.com>
2022-12-08 08:14:27 -0600

    Updated PhoneNumber.h

--- PhoneNumber.h
+++ PhoneNumber.h
@@ -1,16 +1,18 @@
 // PhoneNumber.h
+#pragma once
+
 #include <iostream>
 #include <string>
 
 class PhoneNumber {
-    private:
-        std::string phone_number;   // phone number: e.g. 1(630)123-4567
-    public:
-        PhoneNumber();  // default constructor
-        PhoneNumber(std::string);   // overloaded constructor
+private:
+    std::string phone_number;   // phone number: e.g. 1(630)123-4567
+public:
+    PhoneNumber();  // default constructor
+    PhoneNumber(std::string);   // overloaded constructor
 
-        void setPhoneNumber(std::string);   // mutator method
-        std::string getPhoneNumber();   // accessor method
+    void setPhoneNumber(std::string);   // mutator method
+    std::string getPhoneNumber() const;   // accessor method
 
-        std::string toString(); // for printing an instance of PhoneNumber
+    std::string toString(); // for printing an instance of PhoneNumber
 };
