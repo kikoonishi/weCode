9fc46983e044c0d027a16a38887026b6ab2b5788
kikoonishi <93617903+kikoonishi@users.noreply.github.com>
2022-12-08 17:57:46 -0600

    Update Manager.cpp

--- Manager.cpp
+++ Manager.cpp
@@ -4,9 +4,6 @@
 #include <string>
 #include "Manager.h" // include definition of class Sales from Sales.h
 
-////////TODO need to be changed based on database
-
-//TODO constructor
 
 template <class T>
 Manager<T>::Manager() : Sales(){
@@ -23,52 +20,38 @@ template <class T>
 //add,delete,edit product/service
 void Manager<T>::addProduct(Database<T> productData, Product product){
     //add product to database
-    productData.add(this->product);
+    productData.add(product);
 }
 
 template <class T>
 void Manager<T>::addService(Database<T> serviceData, Service service){
     //add service to database
-    serviceData.add(this->service);
+    serviceData.add(service);
 }
 
 template <class T>
 void Manager<T>::deleteProduct(Database<T> productData, Product product){
     //delete product from daatabase
-    productData.deleteElement(this->product);
-}
-
-template <class T>
-void Manager<T>::deleteProduct(Database<T> productData, std::string name){
-    //get object from db by passing name (should it be index?)
-    Product product = productData.get(name);
-    //delete product from daatabase
-    productData.deleteElement(this->product);
+    productData.remove(this->product);
 }
 
 template <class T>
 void Manager<T>::deleteService(Database<T> serviceData, Service service){
-    //delete service from database
-    serviceData.deleteElement(this->service);
-}
-
-template <class T>
-void Manager<T>::deleteService(Database<T> serviceData, std::string name){
-    //get object from db by passing name (should it be index?)
-    Service service = serviceData.get(name);
-    //delete service from database
     serviceData.deleteElement(this->service);
 }
 
 template <class T>
 void modifyProduct(Database<T> productData, Product product, Product newProduct){
-    //edit product in database
-    productData.modify(product, newProduct);
+    //remove
+    productData.remove(product);
+    //add product in database
+    productData.add(newProduct);
 }
 
 template <class T>
 void Manager<T>::modifyService(Database<T> serviceData, Service service, Service newService){
-    //edit service in database
-    serviceData.modify(service, newService);
+    //remove
+    serviceData.remove(product);
+    //add product in database
+    serviceData.add(newProduct);
 }
-
