419da9b83401c8ea708718b3bcfb1d1a6bfc98c4
kikoonishi <93617903+kikoonishi@users.noreply.github.com>
2022-12-08 18:03:55 -0600

    Update Sales.cpp

--- Sales.cpp
+++ Sales.cpp
@@ -6,10 +6,10 @@
 #include <string>
 #include "Sales.h" // include definition of class Sales from Sales.h
 
-////////TODO need to be changed based on database
+
 template <class T>
 Sales<T>::Sales() : Customer(){
-    //TODO default constructor
+    //default constructor
     setCustomerID("0");
     setCustomerType("Sales");
     ++total_customers;
@@ -28,27 +28,21 @@ void Sales<T>::placeOrder(Database<T> database, Order order){
 template <class T>
 void Sales<T>::deleteOrder(Database<T> database, Order order){
     //delete order from database
-    database.deleteElement(order);
-}
-
-template <class T>
-void Sales<T>::deleteOrder(Database<T> database, std::string ordername){
-    //get object from db by passing ordername (should it be index?)
-    Order order = database.get(ordername);
-    //delete order from database
-    database.deleteElement(order);
+    database.remove(order);
 }
 
 template <class T>
 void Sales<T>::deleteOrder(Database<T> database, Customer customer){
     //delete order from database
-    database.deleteElement(customer);
+    database.remove(customer);
 }
 
 template <class T>
 void Sales<T>::modifyOrder(Database<T> database, Order existingOrder, Order newOrder){
-    //replace existing order to new order
-    database.modify(existingOrder, newOrder);
+    //delete existing order
+    database.remove(existingOrder);
+    //add new order
+    database.add(newOrder);
     
 }
 
