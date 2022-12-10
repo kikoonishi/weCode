c05931d7384cb881cf59f3aedf8130a6544713f9
kikoonishi <93617903+kikoonishi@users.noreply.github.com>
2022-12-10 15:19:23 -0600

    Update Manager.h

--- Manager.h
+++ Manager.h
@@ -5,26 +5,51 @@
 #define MANAGER_H
 
 #include <string>
-#include "Sales.h"
+#include "User.h"
+#include "Account.h"
+#include "Order.h"
 #include "Product.h"
 #include "Service.h"
 #include "Database.h"
 
-template <class T>
-class Manager :public Sales {
+//template <class T>
+class Manager :public User {
+    private:
+        std::string manager_id;
+
     public:
-        //default constructor?????
-        Manager();
-        Manager(): Sales();
-        Manager(std::string, std::string, Account) : Sales(std::string, std::string, Account);//newID, type, userAccount
+        Manager(); // default constructor
+        Manager(std::string, std::string, std::string);//Manager fields + username, password
+
+        void setManagerID(std::string);
+        std::string getManagerID();
 
         //add,delete,edit product/service in database
-        void addProduct(Database<T>, Product);        
+        template <class T>
+        void addProduct(Database<T>, Product);
+        template <class T>        
         void addService(Database<T>, Service);
+        template <class T>
         void deleteProduct(Database<T>, Product);//delete product by Product
+        template <class T>
         void deleteService(Database<T>, Service);//delete product by Service
+        template <class T>
         void modifyProduct(Database<T>, Product, Product);//existring Product to new Product
+        template <class T>
         void modifyService(Database<T>, Service, Service);//existring Service to new Service
+
+
+        //Order
+        template <class T>
+        void placeOrder(Database<T>, Order);
+        template <class T>
+        void deleteOrder(Database<T>, Order); //deleter order by order
+        template <class T>
+        void deleteOrder(Database<T>, std::string); //delete order by ordername
+        template <class T>
+        void modifyOrder(Database<T>, Order, Order); //existed Order and new Order
+        template <class T>
+        void viewOrder(Database<T>); //print database
 
 };
 
