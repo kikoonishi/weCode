246708226472335079666f06f2c06cf7b9f5d0cb
atrafacz <alexrafacz@gmail.com>
2022-12-10 19:08:17 -0600

    Update Sales.cpp

--- Sales.cpp
+++ Sales.cpp
@@ -7,53 +7,51 @@
 #include "Sales.h" // include definition of class Sales from Sales.h
 
 
-template <class T>
-Sales<T>::Sales() : Customer(){
-    //default constructor
-    setCustomerID("0");
-    setCustomerType("Sales");
-    ++total_customers;
+Sales::Sales() : User() {
+    //Sales field
+    setSalesID("1");
 }
 
-template <class T>
-Sales<T>::Sales(std::string newID, std::string type, Account userAccount) : Customer(newID, type, userAccount){}
+Sales::Sales(std::string username, std::string password, Account user_account, std::string new_sales_id) : User(username, password, user_account) {
+    setSalesID(new_sales_id);
+}
+
+void Sales::setSalesID(std::string new_sales_id) {
+    sales_id = new_sales_id;
+}
+
+std::string Sales::getSalesID() const { return sales_id; }
 
 template <class T>
-void Sales<T>::placeOrder(Database<T> database, Order order){
+void Sales::placeOrder(Database<T> database, Order order) {
     //add order to database (ordername, product/service, quantity)
     database.add(order);
 }
 
+//return fields as string
+std::string Sales::toString() {
+    std::string toString = "";
+    toString += "Manager ID: " + sales_id;
+    return toString;
+}
 
 template <class T>
-void Sales<T>::deleteOrder(Database<T> database, Order order){
+void Sales::deleteOrder(Database<T> database, Order order) {
     //delete order from database
     database.remove(order);
 }
 
 template <class T>
-void Sales<T>::deleteOrder(Database<T> database, Customer customer){
-    //delete order from database
-    database.remove(customer);
-}
-
-template <class T>
-void Sales<T>::modifyOrder(Database<T> database, Order existingOrder, Order newOrder){
+void Sales::modifyOrder(Database<T> database, Order existingOrder, Order newOrder) {
     //delete existing order
     database.remove(existingOrder);
     //add new order
     database.add(newOrder);
-    
+
 }
 
 template <class T>
-void Sales<T>::viewOrder(Database<T> database){
-   //get database as string and print
-   std::cout << database.toString() << std::endl; 
+void Sales::viewOrder(Database<T> database) {
+    //get database as string and print
+    database.print();
 }
-
-template <class T>
-std::string toString(Database<T> database){
-    //get database as string and return
-    return database.toString();
-};
