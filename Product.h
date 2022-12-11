8bf28a89d1c604a4e9724b41923637a97701dad4
atrafacz <alexrafacz@gmail.com>
2022-12-10 19:44:28 -0600

    Update Product.h

--- Product.h
+++ Product.h
@@ -4,18 +4,20 @@
 
 class Product
 {
-	private:
-		std::string productName;
-		std::string type;
-		double price;
-	
-	public:
-		Product();
-		Product(std::string name, std::string type, double cost);
-		std::string getName();
-		double getPrice();
-		void setName(std::string name);
-		void setPrice(double cost);
-		std::string toString();
-		Product copy();
+private:
+	std::string productName;
+	std::string type;
+	double price;
+
+public:
+	Product();
+	Product(std::string name, std::string type, double cost);
+	std::string getName() const;
+	double getPrice() const;
+	std::string getType() const;
+	void setName(std::string name);
+	void setPrice(double cost);
+	void setType(std::string type);
+	std::string toString();
+	void copy(Product);
 };
