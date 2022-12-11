4a4251c15cc2a47811cadfe6a759d616e39ddd2e
atrafacz <alexrafacz@gmail.com>
2022-12-11 16:56:30 -0600

    Update Manager.h

--- Manager.h
+++ Manager.h
@@ -8,7 +8,6 @@
 #include "Order.h"
 #include "Product.h"
 #include "Service.h"
-#include "Database.h"
 
 //template <class T>
 class Manager :public User {
@@ -22,23 +21,6 @@ public:
     void setManagerID(std::string);
     std::string getManagerID() const;
     std::string toString();
-
-    //add,delete,edit product/service in database
-    void addProduct(Database<Product>, Product);
-    void addService(Database<Service>, Service);
-    void deleteProduct(Database<Product>, Product);//delete product by Product
-    void deleteService(Database<Service>, Service);//delete product by Service
-    void modifyProduct(Database<Product>, Product, Product);//existring Product to new Product
-    void modifyService(Database<Service>, Service, Service);//existring Service to new Service
-    void viewProduct(Database<Product>);//view database
-    void viewService(Database<Service>);
-
-    //Order
-    void placeOrder(Database<Order>, Order);
-    void deleteOrder(Database<Order>, Order); //deleter order by order
-    void deleteOrder(Database<Order>, std::string); //delete order by ordername
-    void modifyOrder(Database<Order>, Order, Order); //existed Order and new Order
-    void viewOrder(Database<Order>); //print database
 };
 
 #endif
