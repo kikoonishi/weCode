bb8a90771809af7e2ed47f7e82ff64525874b34a
atrafacz <alexrafacz@gmail.com>
2022-12-10 19:09:57 -0600

    Update Manager.cpp

--- Manager.cpp
+++ Manager.cpp
@@ -5,43 +5,57 @@
 #include "Manager.h" // include definition of class Sales from Sales.h
 
 
-template <class T>
-Manager<T>::Manager() : Sales<T>(){
-    //TODO default constructor
-    setCustomerID("0");
-    setCustomerType("Sales");
-    ++total_customers;
+
+Manager::Manager() : User() {    //default constructor
+    //Manager field
+    setManagerID("2");
 }
 
-template <class T>
-Manager<T>::Manager(std::string newID, std::string type, Account userAccount): Sales<T>(newID, type, userAccount){}      //call parent class constructor 
+//call parent class constructor 
+Manager::Manager(std::string username, std::string password, Account user_account, std::string new_manager_id) : User(username, password, user_account) {
+    setManagerID(new_manager_id);
+}
 
+//set id
+void Manager::setManagerID(std::string newID) { manager_id = newID; }
+//get id
+std::string Manager::getManagerID() const { return manager_id; }
+
+//return fields as string
+std::string Manager::toString() {
+    std::string toString = "";
+    toString += "Manager ID: " + manager_id;
+    return toString;
+}
+
+
+//Product/Service
 template <class T>
 //add,delete,edit product/service
-void Manager<T>::addProduct(Database<T> productData, Product product){
+void Manager::addProduct(Database<T> productData, Product product) {
     //add product to database
     productData.add(product);
 }
 
 template <class T>
-void Manager<T>::addService(Database<T> serviceData, Service service){
+void Manager::addService(Database<T> serviceData, Service service) {
     //add service to database
     serviceData.add(service);
 }
 
 template <class T>
-void Manager<T>::deleteProduct(Database<T> productData, Product product){
+void Manager::deleteProduct(Database<T> productData, Product product) {
     //delete product from daatabase
     productData.remove(this->product);
 }
 
 template <class T>
-void Manager<T>::deleteService(Database<T> serviceData, Service service){
+void Manager::deleteService(Database<T> serviceData, Service service) {
     serviceData.deleteElement(this->service);
 }
 
 template <class T>
-void modifyProduct(Database<T> productData, Product product, Product newProduct){
+void Manager::modifyProduct(Database<T> productData, Product product, Product newProduct) {
     //remove
     productData.remove(product);
     //add product in database
@@ -49,9 +63,49 @@ void modifyProduct(Database<T> productData, Product product, Product newProduct)
 }
 
 template <class T>
-void Manager<T>::modifyService(Database<T> serviceData, Service service, Service newService){
+void Manager::modifyService(Database<T> serviceData, Service service, Service newService) {
     //remove
     serviceData.remove(product);
     //add product in database
     serviceData.add(newProduct);
+}
+
+template <class T>
+void Manager::viewProduct(Database<T> productData) {
+    //get database as string and print
+    productData.print();
+}
+
+template <class T>
+void Manager::viewService(Database<T> serviceData) {
+    //get database as string and print
+    serviceData.print();
+}
+
+//Order
+template <class T>
+void Manager::placeOrder(Database<T> orderData, Order order) {
+    //add order to database (ordername, product/service, quantity)
+    orderData.add(order);
+}
+
+template <class T>
+void Manager::deleteOrder(Database<T> orderData, Order order) {
+    //delete order from database
+    orderData.remove(order);
+}
+
+template <class T>
+void Manager::modifyOrder(Database<T> orderData, Order existingOrder, Order newOrder) {
+    //delete existing order
+    orderData.remove(existingOrder);
+    //add new order
+    orderData.add(newOrder);
+
+}
+
+template <class T>
+void Manager::viewOrder(Database<T> orderData) {
+    //get database as string and print
+    orderData.print();
 }
