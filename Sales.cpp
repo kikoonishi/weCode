54ea166bad9aa62fef5190d6801f08628f040a5a
atrafacz <alexrafacz@gmail.com>
2022-12-11 16:58:40 -0600

    Update Sales.cpp

--- Sales.cpp
+++ Sales.cpp
@@ -19,32 +19,9 @@ void Sales::setSalesID(std::string new_sales_id) {
 
 std::string Sales::getSalesID() const { return sales_id; }
 
-void Sales::placeOrder(Database<Order> database, Order order) {
-    //add order to database (ordername, product/service, quantity)
-    database.add(order);
-}
-
 //return fields as string
 std::string Sales::toString() {
     std::string toString = "";
     toString += "Manager ID: " + sales_id;
     return toString;
-}
-
-void Sales::deleteOrder(Database<Order> database, Order order) {
-    //delete order from database
-    database.remove(order);
-}
-
-void Sales::modifyOrder(Database<Order> database, Order existingOrder, Order newOrder) {
-    //delete existing order
-    database.remove(existingOrder);
-    //add new order
-    database.add(newOrder);
-
-}
-
-void Sales::viewOrder(Database<Order> database) {
-    //get database as string and print
-    database.print();
 }
