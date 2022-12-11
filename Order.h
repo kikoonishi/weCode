3541f19128ef4d52837d10ea588da21b371fe215
atrafacz <alexrafacz@gmail.com>
2022-12-10 19:10:29 -0600

    Update Order.h

--- Order.h
+++ Order.h
@@ -8,27 +8,25 @@
 
 class Order
 {
-	private:
-		Product product;// order contains a product or service
-		Service service;// can use this to make a package deal???? Or maybe create an arraylist 
-		std::string orderName;// Might want to have a customer be attatched to the order
-		Customer customer;
+private:
+	Product product;// order contains a product or service
+	Service service;// can use this to make a package deal???? Or maybe create an arraylist 
+	std::string orderName;// Might want to have a customer be attatched to the order
+	Customer customer;
 
-	public:
-		Order();
-		Order(Product p, std::string name, Customer c);
-		Order(Service s, std::string name, Customer c);
-		Order(Product p, Service s, std::string name, Customer c);
-		Product getProduct();
-		Service getService();
-		std::string getOrderName();
-		void setProduct(Product p);
-		void setService(Service s);
-		void setOrderName(std::string name);
-		std::string toString();
-		Order copy();
-
-
-
-
+public:
+	Order();
+	Order(Product p, std::string name, Customer c);
+	Order(Service s, std::string name, Customer c);
+	Order(Product p, Service s, std::string name, Customer c);
+	Product getProduct() const;
+	Service getService() const;
+	std::string getOrderName() const;
+	Customer getCustomer() const;
+	void setProduct(Product p);
+	void setService(Service s);
+	void setOrderName(std::string name);
+	void setCustomer(Customer c);
+	std::string toString();
+	Order copy();
 };
